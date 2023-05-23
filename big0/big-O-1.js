e68b3f1f6f2650ab4533b1813075b484c60fabc3
Jisan-mia <jisanmia47@gmail.com>
2023-05-23 20:29:24 +0600

    big O(1)

---
+++ big0/big-O-1.js
@@ -0,0 +1,12 @@
+const boxes = [1,2,3,4,5]
+
+function findSecondBox(array) {
+  console.log(array[1])
+} 
+
+findSecondBox(boxes)
+
+// whats the big O of the findSecondBox() function
+// well its O(1)
+// because the function takes constant time to perform the task
+// no matter how big array size is, the number of operation of the function is always one
