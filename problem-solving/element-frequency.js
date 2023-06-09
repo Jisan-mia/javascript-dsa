7df419a023bdd278b9b27eb375fcd9bc9210bbc6
Jisan-mia <jisanmia47@gmail.com>
2023-06-09 10:50:16 +0600

    check frequency of every element in an array

--- problem-solving/element-frequency.js
+++ problem-solving/element-frequency.js
@@ -3,5 +3,13 @@ Write a function checkFrequency to check frequency of each element in an array;
 */
 
 function checkFrequency(arr) {
-  
+  const frequencyMap = {}
+
+  for(elm of arr) {
+    frequencyMap[elm] = frequencyMap[elm] + 1 || 1
+  }
+
+  return frequencyMap
 }
+
+console.log(checkFrequency([5, 2, 1, 8, 2, 9, 2, 5]))
