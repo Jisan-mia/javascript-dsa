709753e05235032aa403e07e806dd3e29dfbb5db
Jisan-mia <jisanmia47@gmail.com>
2023-06-12 23:11:04 +0600

    what is linked lists

---
+++ linkedinLists/index.js
@@ -0,0 +1,11 @@
+// Linked Lists
+// it's a lists that is linked
+// there are 2 types of linked lists
+// 1. singly linked lists
+// 2. doubly linked lists
+
+// a singly linked lists contains a set of nodes, each nodes have 2 elements
+// one is the value we want to store and the second is a pointer that is pointing to the next node
+// the first node is called "head" and the last node is called "tail"
+// linked lists are null terminated
+// when a nodes points to null that means that nodes is the tail node
