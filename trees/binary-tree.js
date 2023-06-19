04e1d505fe4859fab381f99e15b6e31b6fa3a1b7
Jisan-mia <jisanmia47@gmail.com>
2023-06-19 23:25:27 +0600

    binary tree --> binary search tree rules

---
+++ trees/binary-tree.js
@@ -0,0 +1,20 @@
+// binary tree rule
+
+/* 
+- each node can only have either 0,1 or 2 nodes
+- and each child can have only one parents
+- lookup O(log N)
+- insert O(log N)
+- delete O(log N)
+*/
+
+/* 
+"Binary Search Tree" is a subset of binary tree
+- its really good at searching
+rules:
+- all child nodes in the tree to the right of the root node must be greater than the current node
+- all child nodes in the tree to the left of the root node must be less than the current node
+- all right side child nodes increases
+- all left side child nodes decreases
+
+*/
