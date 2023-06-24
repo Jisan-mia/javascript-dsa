8fc3cf04ff02d7017b85c4e2a6a9b5d27166b4cc
Jisan-mia <jisanmia47@gmail.com>
2023-06-24 23:18:11 +0600

    bubble sort algo implementation

---
+++ a-sorting/buble-sort.js
@@ -0,0 +1,17 @@
+const numbers = [99, 44, 9, 2, 1, 5, 63, 87, 283, 4, 0]
+
+function bubbleSort(arr) { // O(n^2)
+  const len =  arr.length;
+  for(let i = 0; i < len; i++) {
+    for(let j = 0; j < len; j++) {
+      if(arr[j] > arr[j+1]) {
+        let temp = arr[j]
+        arr[j] = arr[j+1]
+        arr[j+1] = temp
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort(numbers))
---
+++ a-sorting/index.js
@@ -0,0 +1,8 @@
+// different types of sorting
+/* 
+- bubble sort
+- insertion sort
+- selection sort
+- merge sort
+- quick sort
+*/
