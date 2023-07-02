a11c53b4b3ddbf33fee23eff64edd6edf0e950e9
Jisan-mia <jisanmia47@gmail.com>
2023-07-02 23:57:17 +0600

    depth first search inOrder implementation

--- a-searching-traversal/dfs-.js
+++ a-searching-traversal/dfs-.js
@@ -1,1 +1,49 @@
 // DFS --> Depth First Search Algorithm
+
+// binary search algorithm
+const { BinarySearchTree } = require("../trees/binary-tree.js");
+
+
+
+BinarySearchTree.prototype.DFSInOrder = function() {
+  return traverseInOrder(this.root, [])
+}
+
+BinarySearchTree.prototype.DFSPreOrder = function() {
+  return traversePreOrder(this.root, []);
+}
+
+BinarySearchTree.prototype.DFSPostOrder = function() {
+  return traversePostOrder(this.root, []);
+}
+
+
+function traverseInOrder(node, list) {
+  if(node.left) {
+    traverseInOrder(node.left, list)
+  }
+  list.push(node.value)
+  if(node.right) {
+    traverseInOrder(node.right, list)
+  }
+
+  return list;
+} 
+
+/* 
+      9
+  4       20
+1   6   15  170
+*/
+
+const myBST = new BinarySearchTree()
+myBST.insert(9)
+myBST.insert(20);
+myBST.insert(15);
+myBST.insert(170);
+myBST.insert(4);
+myBST.insert(6);
+myBST.insert(1);
+console.log(myBST)
+
+console.log(myBST.DFSInOrder());
